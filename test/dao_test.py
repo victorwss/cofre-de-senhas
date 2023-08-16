bf8d8543693cda5d2b18de1478bcc616ec3b93bc
Victor Williams Stafusa da Silva <victorwsilva@gmail.com>
2023-08-16 14:34:56 -0300

    Preparando mais testes.

--- test/dao_test.py
+++ test/dao_test.py
@@ -1,3 +1,8 @@
-def zzz_test_x1():
-    from cofre_de_senhas.bd.raiz import Raiz
-    pass
+from cofre_de_senhas.bd.raiz import Raiz
+
+def test_x1():
+    Raiz.register_sqlite("test/cofre-teste.db")
+    CategoriaDAOImpl()
+    CofreDeSenhasDAOImpl()
+    SegredoDAOImpl()
+    UsuarioDAOImpl()
