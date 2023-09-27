f407724c43c110992116ead3afa957d7d033e05d
Victor Williams Stafusa da Silva <victorwsilva@gmail.com>
2023-09-27 18:41:06 -0300

    Pipeline de execução.

--- test.bat
+++ test.bat
@@ -1,5 +1,26 @@
+@echo OFF
+
+:TEST
+@echo ON
 mypy .\src .\test
+@echo OFF
+IF NOT %ERRORLEVEL% == 0 GOTO EXIT
+
+:RADON
+@echo ON
 radon cc -nb -s .\src
 radon mi -nb -s .\src
+@echo OFF
+
+:COVERAGE
+@echo ON
 coverage run --branch --source=src -m pytest
+@echo OFF
+IF NOT %ERRORLEVEL% == 0 GOTO EXIT
+
+:REPORT
+@echo ON
 coverage report -m
+@echo OFF
+
+:EXIT
