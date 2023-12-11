1c90a0049b018482e4089ed346ae32ff6550b76f
Victor Williams Stafusa da Silva <victorwsilva@gmail.com>
2023-12-11 19:00:46 -0300

    Usando o requirements.txt.

--- install.sh
+++ install.sh
@@ -1,11 +1,1 @@
-pip install mypy                   --require-virtualenv --timeout 8
-pip install pytest                 --require-virtualenv --timeout 8
-pip install pytest-timeout         --require-virtualenv --timeout 8
-pip install dacite                 --require-virtualenv --timeout 8
-pip install mariadb                --require-virtualenv --timeout 8
-pip install mysql-connector-python --require-virtualenv --timeout 8
-pip install requests               --require-virtualenv --timeout 8
-pip install types-requests         --require-virtualenv --timeout 8
-pip install flask                  --require-virtualenv --timeout 8
-pip install radon                  --require-virtualenv --timeout 8
-pip install coverage               --require-virtualenv --timeout 8
+pip install -r requirements.txt --require-virtualenv --timeout 8
