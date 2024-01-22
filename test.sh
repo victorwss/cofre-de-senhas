d8a3c8d4b1ccfc4048529b572aa763ca1a9c5291
Vítor Capparelli <vitorcapparelli@prefeitura.sp.gov.br>
2024-01-22 17:37:45 -0300

    ci: create job to lint-test

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 mypy ./src ./test
 radon cc -nb -s --no-assert ./src
 radon mi -nb -s             ./src
-flake8                      ./src
-flake8                      ./test
 coverage run --branch --source=src -m pytest --timeout=8
 coverage report -m
