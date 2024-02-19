e1eadc49f6d5a28ca4893d673c59e51afed0171c
Vítor Capparelli <vitorcapparelli@prefeitura.sp.gov.br>
2024-02-19 17:49:22 -0300

    test: remove unnecessary parameters

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@ mypy ./src ./test
 radon cc -nb -s --no-assert ./src
 radon mi -nb -s             ./src
 flake8 ./src ./test
-coverage run --branch --source=src -m pytest --timeout=8
+coverage run
 coverage report -m
