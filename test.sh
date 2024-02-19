d2617586058ce000d869bfb889ccc0ab25310fd0
Vítor Capparelli <vitorcapparelli@prefeitura.sp.gov.br>
2024-02-19 17:39:35 -0300

    ci: fix to merge

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
+mypy ./src ./test
 radon cc -nb -s --no-assert ./src
 radon mi -nb -s             ./src
+flake8 ./src ./test
 coverage run --branch --source=src -m pytest --timeout=8
 coverage report -m
