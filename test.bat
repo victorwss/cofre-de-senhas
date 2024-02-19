963fb842973fc38acdaee878be301fe6445a68ff
Victor Williams Stafusa da Silva <victorwsilva@gmail.com>
2024-02-19 18:56:35 -0300

    Pequena correção.

--- test.bat
+++ test.bat
@@ -10,8 +10,7 @@ IF NOT %ERRORLEVEL% == 0 GOTO EXIT
 @echo ON
 radon cc -nb -s --no-assert .\src
 radon mi -nb -s             .\src
-flake8                      .\src
-flake8                      .\test
+flake8 .\src .\test
 coverage run --branch --source=src -m pytest --timeout=8
 @echo OFF
 IF NOT %ERRORLEVEL% == 0 GOTO EXIT
