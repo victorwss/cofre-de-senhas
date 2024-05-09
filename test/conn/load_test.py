0bc36e3cbbcdcc500b87c2edbe0012c3ae6cf0f0
Victor Williams Stafusa da Silva <victorwsilva@gmail.com>
2024-05-09 16:01:43 -0300

    Ops.

--- test/conn/load_test.py
+++ test/conn/load_test.py
@@ -20,22 +20,27 @@ def _read_all(fn: str) -> str:
     with open(os.getcwd() + "/" + fn, "r", encoding = "utf-8") as f:
         return f.read()
 
+
 def test_simple_create() -> None:
     x: DatabaseConfig = DatabaseConfig("test", {"a": "b", "c": 555})
     assert x.flavor == "test"
     assert x.properties == {"a": "b", "c": 555}
 
+
 def test_bad_create_1() -> None:
     with raises(TypeValidationError):
-        DatabaseConfig(42, {"a": "b", "c": 555})
+        DatabaseConfig(42, {"a": "b", "c": 555})  # type: ignore
+
 
 def test_bad_create_2() -> None:
     with raises(TypeValidationError):
-        DatabaseConfig("foo", [])
+        DatabaseConfig("foo", [])  # type: ignore
+
 
 def test_bad_create_3() -> None:
     with raises(TypeError):
-        DatabaseConfig()
+        DatabaseConfig()  # type: ignore
+
 
 @mark.parametrize("file_name", files_ok)
 def test_from_json_ok(file_name: str) -> None:
@@ -46,6 +51,7 @@ def test_from_json_ok(file_name: str) -> None:
         t: tuple[RAW_DATA, ...] | None = c.fetchone()
         assert t == (1, )
 
+
 @mark.parametrize("file_name", files_ok)
 def test_from_json_file_ok(file_name: str) -> None:
     d: DatabaseConfig = DatabaseConfig.from_file(file_name)
@@ -54,36 +60,44 @@ def test_from_json_file_ok(file_name: str) -> None:
         t: tuple[RAW_DATA, ...] | None = c.fetchone()
         assert t == (1, )
 
+
 def test_from_json_file_bad_file() -> None:
     with raises(FileNotFoundError):
         DatabaseConfig.from_file(do_not_exist)
+
 
 def test_from_json_bad_format() -> None:
     json: str = _read_all(futebol)
     with raises(UnexpectedDataError):
         DatabaseConfig.from_json(json)
 
+
 def test_from_json_file_bad_format() -> None:
     with raises(UnexpectedDataError):
         DatabaseConfig.from_file(futebol)
+
 
 def test_from_json_bad_flavor_type() -> None:
     json: str = _read_all(bad_flavor)
     with raises(WrongTypeError):
         DatabaseConfig.from_json(json)
 
+
 def test_from_json_file_bad_flavor_type() -> None:
     with raises(WrongTypeError):
         DatabaseConfig.from_file(bad_flavor)
+
 
 def test_from_json_bad_json() -> None:
     json: str = _read_all(gibberish)
     with raises(JSONDecodeError):
         DatabaseConfig.from_json(json)
 
+
 def test_from_json_file_bad_json() -> None:
     with raises(JSONDecodeError):
         DatabaseConfig.from_file(gibberish)
+
 
 @mark.parametrize("file_name", files_bad)
 def test_from_json_bad_config_props(file_name: str) -> None:
@@ -91,8 +105,9 @@ def test_from_json_bad_config_props(file_name: str) -> None:
     with raises(BadDatabaseConfigException):
         d.connect()
 
+
 @mark.parametrize("file_name", files_bad)
-def test_from_json_bad_config_props(file_name: str) -> None:
+def test_from_json_file_bad_config_props(file_name: str) -> None:
     json: str = _read_all(file_name)
     d: DatabaseConfig = DatabaseConfig.from_json(json)
     with raises(BadDatabaseConfigException):
